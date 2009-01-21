860ae326d24f2585855ee2db93ca87ad1adda16e
Jarkko Oranen <jarkko.oranen@tut.fi>
2009-01-21 16:01:19 +0200

    Simplify pqueue conj; add seq operation

--- hiredman/pqueue.clj
+++ hiredman/pqueue.clj
@@ -1,28 +1,21 @@
 (ns hiredman.pqueue
-    (:refer-clojure :exclude [first conj])
+    (:refer-clojure :exclude [first conj seq])
     (:require [clojure.core :as cc]))
 
+(defn seq
+  "returns a lazy sequence of the items in the priority queue"
+  [pq]
+  (map second pq))
+
 (defn first
-      "returns the first item in priority queue"
-      [x]
-      (second (cc/first x)))
+  "returns the first item in a priority queue"
+  [x]
+  (cc/first (seq pq)))
 
 (defn conj
-      "add item to priority queue nil is the same as 0
-      [col [priority item]]"
-      ([x f]
-       (apply concat
-              (interpose (list f)
-                         (split-at
-                           (count (take-while #(if (number? (cc/first f))
-                                                 (< (if-let [t (cc/first %)]
-                                                            t
-                                                            0) (inc (cc/first f)))
-                                                 (< (if-let [t (cc/first %)]
-                                                            t
-                                                            0) 1)) x))
-                           x))))
-      ([x f & y]
-       (if y
-         (recur (conj x f) (cc/first y) (rest y))
-         (conj x f))))
+  "adds items to the priority queue. items must be pairs of form
+  [priority value]; nil as a priority is equal to 0."
+  [pq & items]
+  (sort-by #(if-let [x (cc/first %)]
+              x
+              0) (apply cc/conj pq items)))
