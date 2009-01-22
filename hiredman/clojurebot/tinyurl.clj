5fb21f82dd5d46ce1acb33010c15166665d09848
Kevin Downey <hiredman@rincewind.(none)>
2009-01-22 13:22:30 -0800

    "working tinyurl noticer"

--- hiredman/clojurebot/tinyurl.clj
+++ hiredman/clojurebot/tinyurl.clj
@@ -21,12 +21,15 @@
 (def url-reg #"[A-Za-z]+://[^  ^/]+\.[^  ^/]+[^ ]+")
 
 (defn getTinyUrl [url]
-      (with-open [pt (.getContent (java.net.URL. (java.net.URLEncode/encode url)))
+      (with-open [pt (.getContent (java.net.URL. (str "http://tinyurl.com/api-create.php?url=" (java.net.URLEncoder/encode url))))
                   dis (java.io.DataInputStream. pt)]
                  (.readLine dis)))
 
 (defmethod responder ::tiny-url [bot pojo]
-  (println (getTinyUrl (re-find url-reg (:message pojo)))))
+  (println (who pojo))
+  (let [url (re-find url-reg (:message pojo))]
+    (when (> (count url) 30)
+      (try (.sendNotice (:this bot) (who pojo) (getTinyUrl url)) (catch Exception e (println e))))))
 
 
 (add-dispatch-hook 20 (dfn (re-find url-reg (:message msg))) ::tiny-url)
