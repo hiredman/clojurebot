fbc2b8770281ad09817e33b84150580fe326fac3
Kevin Downey <redchin@gmail.com>
2009-01-24 17:00:41 -0800

    begin rewrite of seenx to make it better then ever

--- hiredman/clojurebot/seenx.clj
+++ hiredman/clojurebot/seenx.clj
@@ -1,9 +1,28 @@
 (ns hiredman.clojurebot.seenx
   (:use (hiredman.clojurebot core)))
 
+(def user-db (ref {}))
+
 (defmethod responder ::seenx [bot msg]
   (let [nick (re-find #"[^ ]+" (.replaceFirst (extract-message bot msg) "seen " ""))]
-    (sendMsg-who bot msg (if-let [where (see-nick? bot nick)] (str "I see " nick " on " where) (str "I don't see " nick " anywhere")))))
+    (if (@user-db nick)
+      (sendMsg-who bot msg (str nick " was last seen saying: " (@user-db nick)))
+      (sendMsg-who bot msg (str "no, I have not seen " nick)))))
 
 (add-dispatch-hook (dfn (and (addressed? bot msg)
                              (re-find #"seen .*[^ ]" (:message msg)))) ::seenx)
+
+(defmethod responder ::watcher [bot msg]
+  (dosync
+    (alter user-db assoc (:sender msg)
+           (cond
+             (:join msg)
+              :join
+             (:part msg)
+              :part
+             :else
+              (:message msg))))
+  #(responder bot (assoc msg ::ignore true)))
+
+
+(add-dispatch-hook -31 (dfn (nil? (::ignore msg))) ::watcher)
