4d6b5f8178ab776881a36df3f4239cddb05327b7
Kevin Downey <redchin@gmail.com>
2009-01-24 17:41:41 -0800

    seenx rewritten,  ::watcher responder is example of shooting bot and msg back through responder have you are done consuming it so other behaviours can watch for parts and joins

--- hiredman/clojurebot/seenx.clj
+++ hiredman/clojurebot/seenx.clj
@@ -3,11 +3,23 @@
 
 (def user-db (ref {}))
 
+(defn last-seen-str [nick [m channel ms]]
+      (let [minutes (int (/ (/ (- (:time (bean (java.util.Date.))) ms) 1000) 60))]
+      (cond
+        (= m :join)
+          (str nick " was last seen joining " channel ", " minutes " minutes ago" )
+        (= m :part)
+          (str nick " was last seen parting " channel ", " minutes " minutes ago")
+         :else
+          (str nick " was last seen in " channel ", " minutes " minutes ago saying: " m))))
+
 (defmethod responder ::seenx [bot msg]
   (let [nick (re-find #"[^ ]+" (.replaceFirst (extract-message bot msg) "seen " ""))]
-    (if (@user-db nick)
-      (sendMsg-who bot msg (str nick " was last seen saying: " (@user-db nick)))
-      (sendMsg-who bot msg (str "no, I have not seen " nick)))))
+    (if (.equals (:nick bot) nick)
+      (sendMsg-who bot msg "Of course I have seen myself.")
+      (if (@user-db nick)
+        (sendMsg-who bot msg (last-seen-str nick (@user-db nick)))
+        (sendMsg-who bot msg (str "no, I have not seen " nick))))))
 
 (add-dispatch-hook (dfn (and (addressed? bot msg)
                              (re-find #"seen .*[^ ]" (:message msg)))) ::seenx)
@@ -15,13 +27,16 @@
 (defmethod responder ::watcher [bot msg]
   (dosync
     (alter user-db assoc (:sender msg)
-           (cond
-             (:join msg)
-              :join
-             (:part msg)
-              :part
-             :else
-              (:message msg))))
+           (vector
+             (cond
+               (:join msg)
+                :join
+               (:part msg)
+                :part
+               :else
+                (:message msg))
+             (:channel msg)
+             (:time (bean (java.util.Date.))))))
   #(responder bot (assoc msg ::ignore true)))
 
 
