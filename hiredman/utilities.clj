5fed5116afebdcf81ea4d37582ab670145b5c3af
Kevin Downey <redchin@gmail.com>
2009-02-02 19:54:13 -0800

    mk-interface macro

--- hiredman/utilities.clj
+++ hiredman/utilities.clj
@@ -4,4 +4,8 @@
       (with-open [a (java.io.BufferedReader. (java.io.InputStreamReader. (.getContent (java.net.URL. x))))]
                  (.readLine a)))
 
-
+(defmacro mk-interface [class fn]
+  (let [x (map #(list (symbol (.getName %))
+                      ['& 'x]
+                      (list 'let ['method (keyword (.getName %))] fn)) (.getMethods (eval class)))]
+    `(proxy [~class] [] ~@x)))
