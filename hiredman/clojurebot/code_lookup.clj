2bd037af7a02558560b409d4fdbcf1ef6630a0c9
Kevin Downey <redchin@gmail.com>
2009-02-13 14:42:52 -0800

    type hint in responder ::code-lookup for .split

--- hiredman/clojurebot/code_lookup.clj
+++ hiredman/clojurebot/code_lookup.clj
@@ -28,8 +28,7 @@
         [(:line a) (:file a)]))
 
 (defn make-url [[line file]]
-      (let [google (str google-code-url file "?=" clojurebot-rev "#" line)
-            google (java.net.URLEncoder/encode google)
+      (let [google (java.net.URLEncoder/encode (str google-code-url file "?=" clojurebot-rev "#" line))
             url (str "http://tinyurl.com/api-create.php?url=" google)]
         (get-url url)))
 
@@ -37,8 +36,10 @@
 
 (defmethod responder ::code-lookup [bot msg]
   (let [message (extract-message bot msg)
-        thing (second (.split message " "))]
+        thing (second (.split #^String message " "))]
     (send-out :notice bot (who msg) (str thing ": " (make-url-cached (get-file-and-ln thing))))))
 
 (add-dispatch-hook (dfn (and (addressed? bot msg)
-                             (re-find #"^def " (extract-message bot msg)))) ::code-lookup)
+                             (re-find #"^(def|source) " (extract-message bot msg)))) ::code-lookup)
+
+;(count (re-find #"^(?:def|source) [^ ]+" "source foo bar"))
