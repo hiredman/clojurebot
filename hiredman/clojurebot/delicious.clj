0fb73e072abe9819c95a0b68271278ed65448c2c
Kevin Downey <redchin@gmail.com>
2009-03-26 23:39:30 -0700

    modernize delicious

--- hiredman/clojurebot/delicious.clj
+++ hiredman/clojurebot/delicious.clj
@@ -8,10 +8,18 @@
 (defn post [[user pass] url descr tag]
       (util/shell (str "fetch -o /dev/null https://" user ":" pass "@api.del.icio.us/v1/posts/add?url=" (URLEncoder/encode url) "&description=" (URLEncoder/encode descr) "&tags=" (URLEncoder/encode tag))))
 
-(defmethod core/responder ::delicious [bot pojo]
+;;(defmethod core/responder ::delicious [bot pojo]
+;;  (let [url (re-find url-reg (:message pojo))
+;;        desc (:message pojo)
+;;        tag (str (:sender pojo) " " (:channel pojo))]
+;;    (post (:delicious bot) url desc tag)))
+;;
+;;(core/add-dispatch-hook 20 (core/dfn (and (re-find url-reg (:message msg)) (:channel msg))) ::delicious)
+
+(core/defresponder ::delicious 20
+  (core/dfn (and (re-find url-reg (:message msg))
+                 (:channel msg))) ;;
   (let [url (re-find url-reg (:message pojo))
         desc (:message pojo)
         tag (str (:sender pojo) " " (:channel pojo))]
     (post (:delicious bot) url desc tag)))
-
-(core/add-dispatch-hook 20 (core/dfn (and (re-find url-reg (:message msg)) (:channel msg))) ::delicious)
