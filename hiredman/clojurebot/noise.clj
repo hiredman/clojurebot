2a797e94a51fcc330008d20e570eee0b3ef4afb8
Kevin Downey <redchin@gmail.com>
2009-04-02 16:53:20 -0700

    make the x from 1/x chance a named constant I can futz with it easier

--- hiredman/clojurebot/noise.clj
+++ hiredman/clojurebot/noise.clj
@@ -4,8 +4,10 @@
 
 (def lookup (second (first (filter #(= (first %) :hiredman.clojurebot.core/lookup) (.getMethodTable core/responder)))))
 
+(def wheel 100)
+
 (core/defresponder ::noise 100
-  (fn [& _] (= 1 (rand-int 100))),
+  (fn [& _] (= 1 (rand-int wheel))) ;;
   (when (not (= (:message msg) ""))
     (binding [core/befuddled (constantly nil)]
       (lookup bot msg))))
