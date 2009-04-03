b4afdaf858b293ee4fb881623a97001986738e03
Kevin Downey <redchin@gmail.com>
2009-04-03 13:46:00 -0700

    and random chance for a lmgtfy url

--- hiredman/clojurebot/google.clj
+++ hiredman/clojurebot/google.clj
@@ -3,6 +3,9 @@
   (:use (hiredman utilities))
   (:require [org.danlarkin.json :as json]))
 
+(def lmgtfy "http://lmgtfy.com/?q=")
+
+(def wheel 100)
 
 (defn google [term]
       (json/decode-from-str (get-url (str "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" (java.net.URLEncoder/encode term)))))
@@ -13,10 +16,12 @@
 
 (defmethod responder ::google [bot msg]
   (let [term (.trim (.replaceFirst (extract-message bot msg) "^google " ""))]
-    (let [[num result] (cull (google term))]
-      (sendMsg-who bot msg (str "First, out of " num " results is:"))
-      (sendMsg-who bot msg (:titleNoFormatting result))
-      (sendMsg-who bot msg (:unescapedUrl result)))))
+    (if (= 0 (rand-int wheel))
+      (send-out :msg bot msg (str lmgtfy (java.net.URLEncoder/encode term)))
+      (let [[num result] (cull (google term))]
+        (sendMsg-who bot msg (str "First, out of " num " results is:"))
+        (sendMsg-who bot msg (:titleNoFormatting result))
+        (sendMsg-who bot msg (:unescapedUrl result))))))
 
-(add-dispatch-hook (dfn (and (addressed? bot msg)
+(add-dispatch-hook (dfn (and (:addressed? (meta msg))
                              (re-find #"^google " (extract-message bot msg)))) ::google)
