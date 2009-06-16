5d71009d5e33d327257098b752bca9292dd8c6f3
Kevin Downey <redchin@gmail.com>
2009-06-15 23:30:42 -0700

    add docstring

--- hiredman/clojurebot/delicious.clj
+++ hiredman/clojurebot/delicious.clj
@@ -1,3 +1,4 @@
+;; posts lines containing urls to a delicious account
 (ns hiredman.clojurebot.delicious
   (:require [hiredman.clojurebot.core :as core]
             [hiredman.utilities :as util])
@@ -5,18 +6,12 @@
 
 (def url-reg #"[A-Za-z]+://[^  ^/]+\.[^  ^/]+[^ ]+")
 
-(defn post [[user pass] url descr tag]
-      (util/shell (str "fetch -o /dev/null https://" user ":" pass "@api.del.icio.us/v1/posts/add?url=" (URLEncoder/encode url) "&description=" (URLEncoder/encode descr) "&tags=" (URLEncoder/encode tag))))
+(defn post
+  "posts a url to the delicious account of [user pass]"
+  [[user pass] url descr tag]
+  (util/shell (str "fetch -o /dev/null https://" user ":" pass "@api.del.icio.us/v1/posts/add?url=" (URLEncoder/encode url) "&description=" (URLEncoder/encode descr) "&tags=" (URLEncoder/encode tag))))
 
-;;(defmethod core/responder ::delicious [bot pojo]
-;;  (let [url (re-find url-reg (:message pojo))
-;;        desc (:message pojo)
-;;        tag (str (:sender pojo) " " (:channel pojo))]
-;;    (post (:delicious bot) url desc tag)))
-;;
-;;(core/add-dispatch-hook 20 (core/dfn (and (re-find url-reg (:message msg)) (:channel msg))) ::delicious)
-
-(core/defresponder ::delicious 20
+(core/defresponder ::delicious 21
   (core/dfn (and (re-find url-reg (:message msg))
                  (:channel msg))) ;;
   (let [url (re-find url-reg (:message msg))
