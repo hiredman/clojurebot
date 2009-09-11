e51057caf1e2c6a92844eb573e8a10d45d841a1a
Kevin Downey <redchin@gmail.com>
2009-09-11 09:29:40 -0700

    change from ">" to eval scala code to "~scala"

--- hiredman/clojurebot/simplyscala.clj
+++ hiredman/clojurebot/simplyscala.clj
@@ -11,7 +11,12 @@
           util/get-url (.split "res0: ") rest ((partial apply str))))
 
 (core/defresponder ::scala 0
-                   (core/dfn (.startsWith (core/extract-message bot msg) ">"))
-                   (core/new-send-out bot :msg msg (scala-eval (.substring (core/extract-message bot msg) 1))))
+  (core/dfn (and (:addressed? (meta msg)) (.startsWith (core/extract-message bot msg) "scala")))
+  (core/new-send-out bot :msg msg
+    (scala-eval
+      (let [m (core/extract-message bot msg)]
+        (if (.startsWith m "scala")
+          (.replaceAll m "^scala" "")
+          (.substring m 1))))))
 
 ;(core/remove-dispatch-hook ::scala)
