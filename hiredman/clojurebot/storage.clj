a94d0b5d739216dad1af76442a69be508169f577
Kevin Downey <redchin@gmail.com>
2009-10-14 21:45:40 -0700

    whoops, lost a paren somewhere

--- hiredman/clojurebot/storage.clj
+++ hiredman/clojurebot/storage.clj
@@ -1,5 +1,5 @@
 (ns hiredman.clojurebot.storage
-  (:require [hiredman.triples :as trip])
+  (:require [hiredman.triples :as trip]))
 
 (defn db-name [bot]
   (let [name (str (:dict-dir bot) (:nick bot) ".db")]
