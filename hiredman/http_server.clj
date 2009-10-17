f9c398e0447f85fb0a023d5267a47694a72a641e
Kevin Downey <redchin@gmail.com>
2009-10-17 02:15:02 -0700

    mini webserver

---
+++ hiredman/http_server.clj
@@ -0,0 +1,47 @@
+(ns hiredman.http-server
+  (:import (java.io InputStreamReader PrintWriter BufferedReader)
+           (java.net ServerSocket URL)))
+
+(def running true)
+
+(defn- client-action [fn reader writer]
+  (prn :client-action)
+  (let [result (fn reader)]
+    (binding [*out* writer]
+      (println "HTTP/1.0 " (:status result))
+      (println "Server: a Simple Server")
+      (println "Content-type: " (:content-type result))
+      (println "Content-length: " (-> result :body .getBytes count))
+      (println "")
+      (println (:body result))))
+  (doseq [x [reader writer]] (.close x)))
+
+(defn- handle-client [fn client-socket]
+  (prn :handle-client)
+  (future
+    (client-action fn
+                   (-> client-socket .getInputStream)
+                   (-> client-socket .getOutputStream PrintWriter.))))
+
+(defn- server-action [fn server-socket]
+  (prn :server-action)
+  (when running (send-off *agent* server-action server-socket))
+  (try
+    (handle-client fn (.accept server-socket))
+    (catch Exception e
+      (prn e)))
+  fn)
+
+(defn http-server [fn portn]
+  (send-off (agent fn)
+            server-action
+            (ServerSocket. portn)))
+
+(defn put [stuff url]
+  (let [con (doto (.openConnection (URL. url))
+              (.setDoInput true)
+              (.setDoOutput true))]
+    (with-open [wtr (-> con .getOutputStream PrintWriter.)]
+      (binding [*out* wtr] (print stuff) (flush)))
+    (with-open [rdr (-> con .getInputStream InputStreamReader. BufferedReader.)]
+      (apply str (line-seq rdr)))))
