78eb1731c80438a0d7cebc7ebef7c61ccc43c450
Hubert Iwaniuk <neotyk@antares-mac.local>
2009-12-24 00:03:33 +0800

    moved sources to src and create project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject
+  clojurebot "0.1.0-SNAPSHOT"
+  :description "An IRC bot written in Clojure"
+  :dependencies
+    [
+     [org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
