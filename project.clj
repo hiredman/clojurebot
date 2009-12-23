0f23f8f9dcb2da8bcb60d1f68d6cdabecf6399a1
Hubert Iwaniuk <neotyk@antares-mac.local>
2009-12-24 00:36:31 +0800

    only FnParse to sort and dependencies are solved

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :dependencies
     [
      [org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+     [org.clojars.snuxoll/clojureql "1.0.0"]
+     [pircbot/pircbot "1.4.2"]
+     [org.danlarkin/clojure-json "1.1-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
