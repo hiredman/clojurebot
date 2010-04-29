e8e4d71107eed488e800765c73c6b399837238c4
Kevin Downey <redchin@gmail.com>
2010-04-29 09:22:04 -0700

    depending on hortnetq, but don't actually use it anywhere

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject
   clojurebot "0.1.0-SNAPSHOT"
+  :repositories {"jboss" "http://repository.jboss.com/maven2/"}
   :description "An IRC bot written in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
@@ -10,5 +11,8 @@
                  [jivesoftware/smack "3.0.4"]
                  [org.clojars.hiredman/fnparse "2.2.4"]
                  [org.clojars.thnetos/opennlp "0.0.3"]
+                 [org.hornetq/hornetq-core-client "2.0.0.GA"]
+                 [org.hornetq/hornetq-transports "2.0.0.GA"]
+                 [org.jboss.netty/netty "3.1.0.GA"]
                  [jivesoftware/smackx "3.0.4"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
