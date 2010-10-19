c929d9d9168b112a0b46cd36debe51c538c7d1cd
Kevin Downey (hiredman) <redchin@gmail.com>
2010-10-18 21:14:27 -0700

    fix a-deref

--- src/clojurebot/conduit.clj
+++ src/clojurebot/conduit.clj
@@ -4,6 +4,13 @@
 (defn a-deref [proc-ref]
   (assoc pass-through
     :type :deref
-    :reply (fn [x] ((:reply @proc-ref) x))
-    :no-reply (fn [x] ((:reply @proc-ref) x))
-    :scatter-gather (fn [x] ((:scatter-gather @proc-ref) x))))
+    :reply (fn this-fn [x]
+             (let [[new-value new-fn] ((:reply @proc-ref) x)]
+               [new-value this-fn]))
+    :no-reply (fn this-fn [x]
+                (let [[new-value new-fn] ((:reply @proc-ref) x)]
+                  [new-value this-fn]))
+    :scatter-gather (fn this-fn [x]
+                      (fn []
+                        (let [[new-value new-fn] (((:scatter-gather @proc-ref) x))]
+                          [new-value this-fn])))))
