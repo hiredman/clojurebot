a7bfbc8edc18acb58e4f8bd806487d14a8bbd84e
Kevin Downey (hiredman) <redchin@gmail.com>
2011-03-12 13:47:36 -0800

    whitespace fixes

--- src/clojurebot/conduit.clj
+++ src/clojurebot/conduit.clj
@@ -9,11 +9,12 @@
              (let [[new-value new-fn] ((:reply @proc-ref) x)]
                [new-value this-fn]))
     :no-reply (fn this-fn [x]
-                (let [[new-value new-fn] ((:reply @proc-ref) x)]
+                (let [[new-value new-fn] ((:no-reply @proc-ref) x)]
                   [new-value this-fn]))
     :scatter-gather (fn this-fn [x]
                       (fn []
-                        (let [[new-value new-fn] (((:scatter-gather @proc-ref) x))]
+                        (let [[new-value new-fn]
+                              (((:scatter-gather @proc-ref) x))]
                           [new-value this-fn])))))
 
 (defn a-if [a b c]
