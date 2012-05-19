2d784dbcff6817c527dff0ed7b3176aa10c71959
Kevin Downey <redchin@gmail.com>
2012-05-19 01:14:17 -0700

    Ah Hah!

--- src/clojurebot/boot.clj
+++ src/clojurebot/boot.clj
@@ -13,38 +13,19 @@
                [(io/file (System/getProperty "user.dir")
                          "modules")])))
 
-(defn evil [cl form-str]
-  (read-string
-   (let [old-cl (.getContextClassLoader (Thread/currentThread))]
-     (try
-       (.setContextClassLoader (Thread/currentThread) cl)
-       (let [rt (.loadClass cl "clojure.lang.RT")
-             compiler (.loadClass cl "clojure.lang.Compiler")
-             var- (fn [s]
-                    (wh/method
-                     rt :var [String String] nil (namespace s) (name s)))
-             class (fn [x] (.loadClass cl (name x)))
-             deref (fn [x] (wh/method (.getClass x) :deref [] x))
-             invoke (fn [x &  args] (wh/method (.getClass x) :invoke []))
-             read-string (fn [s]
-                           (wh/method rt :readString [String] nil s))
-             eval (fn [f]
-                    (wh/method compiler :eval [Object] nil f))]
-         (eval (read-string (format "(pr-str %s)" form-str))))
-         (finally
-           (.setContextClassLoader (Thread/currentThread) old-cl))))))
-
 (defonce server (vc/create-server))
 
 (defn f []
-  (let [l (loader)
-        m (.loadModule l (ModuleIdentifier/create "clojurebot.legacy" "main"))]
-    (evil (.getClassLoader m)
-          (pr-str
-           `(do
-              (require 'clojurebot.legacy)
-              (future ((resolve 'clojurebot.legacy/-main)))
-              nil)))))
+  (future
+    (let [l (loader)
+          m (.loadModule l (ModuleIdentifier/create "clojurebot.legacy" "main"))
+          cl (.getClassLoader m)]
+      (let [old-cl (.getContextClassLoader (Thread/currentThread))]
+        (try
+          (.setContextClassLoader (Thread/currentThread) cl)
+          (.run m nil)
+          (finally
+            (.setContextClassLoader (Thread/currentThread) old-cl)))))))
 
 (defmacro remote-do [& forms]
   (let [reply-queue (name (gensym 'reply))]
