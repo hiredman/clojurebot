91560d052a8a527b1d8d2dd8f685682a476a75a9
Kevin Downey <redchin@gmail.com>
2013-02-17 00:56:44 -0800

    clojurebot-facts subproject

---
+++ clojurebot-facts/project.clj
@@ -0,0 +1,11 @@
+(defproject clojurebot-facts "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.1.8"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [sonian/carica "1.0.2"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler clojurebot.facts/handler})
