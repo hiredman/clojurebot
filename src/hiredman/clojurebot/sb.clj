693b0610e68185555b02bdf583f267343b49d3e4
Kevin Downey <redchin@gmail.com>
2013-02-17 00:45:06 -0800

    clean out old sandbox stuff, replaced by clojurebot-eval service

--- src/hiredman/clojurebot/sb.clj
+++ src/hiredman/clojurebot/sb.clj
@@ -1,47 +1,6 @@
 (ns hiredman.clojurebot.sb
-  (:use (hiredman.clojurebot core)
-        (hiredman sandbox)
-        [clojure.tools.logging :only [info]])
-  (:require [clj-http.client :as http]))
-
-(let [cl-cache (atom {})]
-  (defn cl [clojure-jar]
-    (if-let [[ctime cl] (get @cl-cache clojure-jar)]
-      (if (> (- (System/currentTimeMillis)
-                (* 10 60 1000))
-             ctime)
-        (do
-          (swap! cl-cache dissoc clojure-jar)
-          (recur clojure-jar))
-        cl)
-      (doto (if clojure-jar
-              (java.security.AccessController/doPrivileged
-               (reify
-                 java.security.PrivilegedAction
-                 (run [_]
-                   (info "new classloader")
-                   (let [bootcp clojure-jar
-                         cp (.split bootcp ":")
-                         cp (for [c cp] (java.net.URL.
-                                         (format "file://%s" c)))
-                         cp (into-array java.net.URL cp)]
-                     (java.net.URLClassLoader. cp nil)))))
-              (.getClassLoader clojure.lang.RT))
-        ;; make sure RT is loaded and inited before we try and use it
-        ;; in the sandbox
-        ((fn [cl]
-           (java.security.AccessController/doPrivileged
-            (reify
-              java.security.PrivilegedAction
-              (run [_]
-                (try
-                  (evil cl "(+ 1 2)")
-                  (catch java.lang.NoClassDefFoundError e
-                    (swap! cl-cache dissoc clojure-jar)
-                    (throw e))))))))
-        ((fn [cl]
-           (swap! cl-cache assoc clojure-jar
-                  [(System/currentTimeMillis) cl])))))))
+  (:require [clj-http.client :as http]
+            [hiredman.clojurebot.core :as cc]))
 
 (defn naughty-forms? [strang]
   (let [nf #{"catch" "finally" "clojure.asm" "hiredman.clojurebot"
@@ -51,30 +10,21 @@
 (defn eval-request? [{:keys [message]}]
   (and message (re-find #"^," (.trim message))))
 
-(let [n (str (java.util.UUID/randomUUID))]
-  (defn eval-message [{:keys [message sender config] :as bag}]
-    (if (and (not (naughty-forms? message))
-             (not= sender "itistoday")
-             (not= sender "Lajla")
-             (not= sender "LauJensen"))
-      (let [result (eval-in-box (.replaceAll message "^," "")
-                                (:sandbox-ns config 'sandbox)
-                                (cl (config :clojure-jar))
-                                n)]
-        (if (vector? result)
-          result
-          (.replace (str result) "(NO_SOURCE_FILE:0)" "")))
-      (str sender ": " (befuddled)))))
-
 (defn eval-message [{:keys [message sender config] :as bag}]
-  (let [{:keys [body]} (http/post (config :evaluator)
-                                  {:form-params {:expression (.replaceAll message "^," "")
-                                                 :befuddled ::befuddled}
-                                   ;; 10 seconds
-                                   :socket-timeout (* 1000 10)
-                                   :conn-timeout (* 1000 10)})
-        {:keys [stdout stderr result]} (read-string body)]
-    (if (or (= result (pr-str ::befuddled))
-            (= result (prn-str ::befuddled)))
-      (str sender ": " (befuddled))
-      [stdout stderr result])))
+  (if (and (not (naughty-forms? message))
+           (not= sender "itistoday")
+           (not= sender "Lajla")
+           (not= sender "LauJensen"))
+    (let [{:keys [body] :as result} (http/get (config :evaluator)
+                                              {:query-params {:expression (.replaceAll message "^," "")
+                                                              :befuddled (pr-str ::befuddled)}
+                                               ;; 10 seconds
+                                               :socket-timeout (* 1000 10)
+                                               :conn-timeout (* 1000 10)})
+          _ (prn result)
+          {:keys [stdout stderr result]} (read-string body)]
+      (if (or (= result (pr-str ::befuddled))
+              (= result (prn-str ::befuddled)))
+        (cc/befuddled)
+        [stdout stderr result]))
+    (str sender ": " (cc/befuddled))))
