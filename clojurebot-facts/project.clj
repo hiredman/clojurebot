0219e56a0f6e552f2aebe82fcec90c5261f331ab
Kevin Downey <redchin@gmail.com>
2013-08-13 17:26:00 -0700

    use the updated fnparse, and move to a newer clojure version for facts

--- clojurebot-facts/project.clj
+++ clojurebot-facts/project.clj
@@ -3,16 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.1.8"]
                  [org.clojure/tools.logging "0.2.4"]
                  [org.apache.derby/derby "10.8.1.2"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [org.clojars.hiredman/fnparse "2.2.4"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+                 [factual/fnparse "2.3.0"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
-                 [clj-http "0.6.4"
-                  :exclude [cheshire]]
+                 [clj-http "0.6.4" :exclude [cheshire]]
                  [clojure-opennlp "0.1.9"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler clojurebot.facts/handler})
