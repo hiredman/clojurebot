1598f091b2d330ef4572967d12a3cc68c069f876
Kevin Downey <redchin@gmail.com>
2013-08-14 10:39:34 -0700

    make facts logging not horrible

--- clojurebot-facts/project.clj
+++ clojurebot-facts/project.clj
@@ -5,12 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.1.8"]
-                 [org.clojure/tools.logging "0.2.4"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [org.apache.derby/derby "10.8.1.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [factual/fnparse "2.3.0"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [clj-http "0.6.4" :exclude [cheshire]]
-                 [clojure-opennlp "0.1.9"]]
+                 [clojure-opennlp "0.1.9"]
+                 [org.clojure/core.logic "0.8.3"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 ;; logging
+                 [ch.qos.logback/logback-classic "1.0.9"]
+                 [ch.qos.logback/logback-core "1.0.9"]
+                 [org.slf4j/jcl-over-slf4j "1.7.2"]
+                 [com.thelastcitadel/m29 "0.1.0"]]
   :plugins [[lein-ring "0.8.2"]]
-  :ring {:handler clojurebot.facts/handler})
+  :ring {:handler clojurebot.facts/handler
+         :init clojurebot.facts/init})
