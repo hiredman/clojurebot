3157acbf21bb7432126f4b6d563caa4c0dcbad02
Kevin Downey <redchin@gmail.com>
2013-08-14 19:43:04 -0700

    clean up, clean up

--- clojurebot-facts/src/clojurebot/infer.clj
+++ clojurebot-facts/src/clojurebot/infer.clj
@@ -1,77 +1,89 @@
 (ns clojurebot.infer
-  (:require [clojure.core.logic :as logic]
-            [clojure.core.logic.protocols :as lp]
-            [clojurebot.triples :as trip]))
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic.protocols :as lp]
+            [clojurebot.triples :as trip]
+            [clojure.core.logic :refer [fresh
+                                        ==
+                                        conde
+                                        !=
+                                        pred
+                                        run*
+                                        tabled
+                                        lvar?
+                                        to-stream]]))
 
-(declare db)
+(declare db infer)
+
+(defn reason-forwards [o subject predicate object level]
+  (fresh [s' p' o']
+    (== s' o)
+    (db s' p' o')
+    (conde
+     [(== object o')]
+     [(infer s' p' o' subject predicate object (inc level))])))
+
+(defn reason-backwards [o subject predicate object level]
+  (fresh [s' p' o']
+    (== o' o)
+    (!= s' subject)
+    (db s' p' o')
+    (conde
+     [(== object s')]
+     ;; new "subject", so re-query the db and recurse
+     [(fresh [s'' p'' o'']
+        (== s' s'')
+        (db s'' p'' o'')
+        (conde
+         [(== object o'')]
+         [(infer s'' p'' o'' subject predicate object (inc level))]))])))
 
 (defn infer [s p o subject predicate object level]
-  (logic/fresh []
-    (logic/pred level (partial > 3))
-    (logic/conde
+  (fresh []
+    (pred level (partial > 3))
+    (conde
      ;; walk down the chain
-     [(logic/fresh [s' p' o']
-        (logic/== s' o)
-        (db s' p' o')
-        (logic/conde
-         [(logic/== object o')]
-         [(infer s' p' o' subject predicate object (inc level))]))]
+     [(reason-forwards o subject predicate object level)]
      ;; walk up the chain
-     [(logic/fresh [s' p' o']
-        (logic/== o' o)
-        (logic/!= s' subject)
-        (db s' p' o')
-        (logic/conde
-         [(logic/== object s')]
-         ;; new "subject", so re-query the db and recurse
-         [(logic/fresh [s'' p'' o'']
-            (logic/== s' s'')
-            (db s'' p'' o'')
-            (logic/conde
-             [(logic/== object o'')]
-             [(infer s'' p'' o'' subject predicate object (inc level))]))]))])))
+     [(reason-backwards o subject predicate object level)])))
 
 (defn respondo [input]
   (distinct
-   (logic/run* [q]
-     (logic/fresh [subject predicate object inferred p o]
-       (logic/== q {:subject subject
-                    :predicate predicate
-                    :object object
-                    :infered? inferred})
-       (logic/!= subject object)
+   (run* [q]
+     (fresh [subject predicate object inferred p o]
+       (== q {:subject subject
+              :predicate predicate
+              :object object
+              :infered? inferred})
+       (!= subject object)
+       (== subject input)
+       (== predicate p)
        (db input p o)
-       (logic/== subject input)
-       (logic/== predicate p)
-       (logic/conde
-        [(logic/== [subject predicate object inferred]
-                   [input p o false])]
-        [(logic/== inferred true)
+       (conde
+        [(== [subject predicate object inferred]
+             [input p o false])]
+        [(== inferred true)
          (infer input p o subject predicate object 0)])))))
 
 (defn db* [subject predicate object]
-  (fn [a]
-    (logic/to-stream
-     (for [rec (let [s (lp/walk a subject)
-                     s (if (logic/lvar? s)
-                         :s
-                         s)
-                     p (lp/walk a predicate)
-                     p (if (logic/lvar? p)
-                         :p
-                         p)
-                     o (lp/walk a object)
-                     o (if (logic/lvar? o)
-                         :o
-                         o)]
-                 (trip/query s p o))]
-       (trampoline
-        (logic/fresh []
-          (logic/== (:subject rec) subject)
-          (logic/== (:predicate rec) predicate)
-          (logic/== (:object rec) object))
-        a)))))
+  (letfn [(f [a x]
+            (let [s (lp/walk a x)]
+              (if (lvar? s)
+                (keyword (name (gensym 'x)))
+                s)))]
+    (fn [a]
+      (let [s (f a subject)
+            p (f a predicate)
+            o (f a object)]
+        (assert (or (not (keyword? s)) (not (keyword? o))))
+        (to-stream
+         (for [rec (trip/query s p o)]
+           (trampoline
+            (fresh []
+              (== (:subject rec) subject)
+              (== (:predicate rec) predicate)
+              (== (:object rec) object))
+            a)))))))
 
 (def db
-  (logic/tabled [subject predicate object]
-                (db* subject predicate object)))
+  (tabled [subject predicate object]
+    (db* subject predicate object)))
