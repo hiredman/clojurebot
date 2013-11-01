75a1a49b82d4d39d8566f6488faa38a746bf3934
Kevin Downey <redchin@gmail.com>
2013-11-01 12:58:05 -0700

    clean up inference

--- clojurebot-facts/src/clojurebot/infer.clj
+++ clojurebot-facts/src/clojurebot/infer.clj
@@ -14,19 +14,25 @@
 
 (declare db infer)
 
-(defn reason-forwards [o subject predicate object level]
+(defn synthesize-forwards [o subject predicate object level]
   (fresh [s' p' o']
     (== s' o)
     (db s' p' o')
     (conde
+     [(== p' "is")]
+     [(== p' "are")])
+    (conde
      [(== object o')]
      [(infer s' p' o' subject predicate object (inc level))])))
 
-(defn reason-backwards [o subject predicate object level]
+(defn synthesize-backwards [o subject predicate object level]
   (fresh [s' p' o']
     (== o' o)
     (!= s' subject)
     (db s' p' o')
+    (conde
+     [(== p' "is")]
+     [(== p' "are")])
     (conde
      [(== object s')]
      ;; new "subject", so re-query the db and recurse
@@ -34,35 +40,54 @@
         (== s' s'')
         (db s'' p'' o'')
         (conde
+         [(== p'' "is")]
+         [(== p'' "are")])
+        (conde
          [(== object o'')]
          [(infer s'' p'' o'' subject predicate object (inc level))]))])))
 
+;; with a better understanding of the edges of the graph (called the
+;; predicate here) other types of synthesis should be possible
+;; if a > b and b > c then a > c sorts of things
+;; (fresh [a b c]
+;;   (db a ">" b)
+;;   (db b ">" c)
+;;   (== [subject predicate object] [a ">" c]))
 (defn infer [s p o subject predicate object level]
   (fresh []
     (pred level (partial > 3))
     (conde
      ;; walk down the chain
-     [(reason-forwards o subject predicate object level)]
+     [(synthesize-forwards o subject predicate object level)]
      ;; walk up the chain
-     [(reason-backwards o subject predicate object level)])))
+     [(synthesize-backwards o subject predicate object level)]
+     [(fresh [a b c]
+        (== a s)
+        (conde
+         [(db a ">" b)]
+         [(db b "<" a)])
+        (conde
+         [(db b ">" c)]
+         [(db c "<" b)])
+        (== [subject predicate object] [a ">" c]))])))
 
 (defn respondo [input]
   (distinct
    (run* [q]
-     (fresh [subject predicate object inferred p o]
-       (== q {:subject subject
-              :predicate predicate
-              :object object
-              :infered? inferred})
-       (!= subject object)
-       (== subject input)
-       (== predicate p)
-       (db input p o)
-       (conde
-        [(== [subject predicate object inferred]
-             [input p o false])]
-        [(== inferred true)
-         (infer input p o subject predicate object 0)])))))
+         (fresh [subject predicate object inferred p o]
+           (== q {:subject subject
+                  :predicate predicate
+                  :object object
+                  :infered? inferred})
+           (!= subject object)
+           (== subject input)
+           (== predicate p)
+           (db input p o)
+           (conde
+            [(== [subject predicate object inferred]
+                 [input p o false])]
+            [(== inferred true)
+             (infer input p o subject predicate object 0)])))))
 
 (defn db* [subject predicate object]
   (letfn [(f [a x]
@@ -86,4 +111,4 @@
 
 (def db
   (tabled [subject predicate object]
-    (db* subject predicate object)))
+          (db* subject predicate object)))
